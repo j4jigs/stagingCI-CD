18ee50cbe1f3b867bef93cf87c4a92dcc6843f4a
jignesharies <jignesh@aries.com>
2025-04-21 20:35:13 +0530

    added new instance

--- terraform/aws/staging/main.tf
+++ terraform/aws/staging/main.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "example" {
   }
 }
 
-resource "aws_instance" "example1" {
+#resource "aws_instance" "example1" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
@@ -42,5 +42,14 @@ resource "aws_instance" "example2" {
 
   tags = {
     Name = "SimpleEC2 instance super"
+  }
+}
+
+resource "aws_instance" "example4" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "SimpleEC2 instance super fine"
   }
 }
