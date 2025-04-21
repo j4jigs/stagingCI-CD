ebe91130c8466127c2a4b02198402ee8d5185b35
jignesharies <jignesh@aries.com>
2025-04-21 15:45:55 +0530

    updated main

--- terraform/aws/staging/main.tf
+++ terraform/aws/staging/main.tf
@@ -29,5 +29,4 @@ resource "aws_instance" "example" {
 
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "my-unique-bucket-name-12345" # Bucket name must be globally unique
-  acl    = "private"
-}  
+ }  
