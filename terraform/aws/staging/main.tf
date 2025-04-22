2b896252fc4d869dd517f56bfde0a43b069c53f4
jignesharies <jignesh@aries.com>
2025-04-22 11:42:41 +0530

    11:42

--- terraform/aws/staging/main.tf
+++ terraform/aws/staging/main.tf
@@ -36,3 +36,11 @@ resource "aws_instance" "example1" {
   }
 }
 
+resource "aws_instance" "example2" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "SimpleEC2 instance super nine"
+  }
+}
